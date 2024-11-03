fbf0c1c1d9d0338de9b29225f7f4b0435ec7a5de
Paulo André <pauloandre0011@gmail.com>
2024-11-03 16:27:47 -0300

    Add icons

---
+++ src/components/Icons/index.tsx
@@ -0,0 +1,6 @@
+export { HomeIcon } from "./icons/HomeIcon";
+export { ExitIcon } from "./icons/ExitIcon";
+export { PrizeIcon } from "./icons/PrizeIcon";
+export { RouteIcon } from "./icons/RouteIcon";
+export { GamepadIcon } from "./icons/GamepadIcon";
+export { FaceHappyIcon } from "./icons/FaceHappyIcon";
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,7 +1,9 @@
+import { HomeIcon, GamepadIcon, FaceHappyIcon, RouteIcon, PrizeIcon } from '@/components';
+
 export const Navbar = () => {
 
   return (
-    <nav className="flex flex-col h-screen gap-4 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2">
+    <nav className="flex flex-col h-screen gap-4 bg-slate-900 border-r border-indigo-400/40 hover:border-indigo-400/80 w-72 p-2 text-slate-300">
       <div className="p-2">
         <img 
           src="https://emersonbroga.com/e/assets/emersonbroga-logo-name-pink.png" alt="logo" 
@@ -10,13 +12,23 @@ export const Navbar = () => {
       </div>
       
       <ul className="flex-grow my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className="my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer">Home</li>
-        <li className="my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer">Games</li>
-        <li className="my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer">Top 10</li>
-        <li className="my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer">Walkthroughs</li>
+        <li className="flex gap-2 items-center my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <HomeIcon className='w-4 h-4' />Home
+        </li>
+        <li className="flex gap-2 items-center my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <GamepadIcon className='w-4 h-4' />Games
+        </li>
+        <li className="flex gap-2 items-center my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <PrizeIcon className='w-4 h-4' />Top 10
+        </li>
+        <li className="flex gap-2 items-center my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <RouteIcon className='w-4 h-4' />Walkthroughs
+        </li>
       </ul>
       <ul className="my-4 border-t border-indigo-400/20 hover:border-indigo-400/40">
-        <li className="my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer">User</li>
+        <li className="flex gap-2 items-center my-2 rounded-lg bg-slate-transparent p-2 hover:bg-slate-800 cursor-pointer hover:text-slate-100">
+          <FaceHappyIcon className='w-4 h-4' />User
+        </li>
       </ul>
     </nav>
   )
