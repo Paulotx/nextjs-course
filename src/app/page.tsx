393692e59c9db44e6ddf7ca4505915f5d9c1b6e3
Paulo André <pauloandre0011@gmail.com>
2024-11-24 10:18:26 -0300

    Init prisma model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,13 @@ export default function Home() {
       <div className="container mx-auto mt-6 px-6">
         <div className="grid grid-cols-4 gap-4 h-[30vh]">
           {articles.splice(-4).map(article => (
-            <div key={article.id} className="flex-center relative overflow-hidden">
+            <div key={article.id} className="flex-center relative overflow-hidden rounded-md">
               <div className='h-full w-full relative'>
                 <Image 
                   fill
                   alt={article.title} 
                   src={`/assets/images/articles/${article.image}`} 
-                  className="w-full h-full object-cover" 
+                  className="w-full h-full object-cover rounded-b-lg" 
                 />
               </div>
 
@@ -33,7 +33,7 @@ export default function Home() {
         <div className="grid grid-cols-12 gap-4">
           <div className="col-span-8 flex flex-col gap-4">
             {articles.map(article => (
-              <div key={article.id} className="flex bg-slate-900 rounded-md h-[220px]">
+              <div key={article.id} className="flex bg-slate-900 rounded-md h-[200px] 2xl:h-[220px]">
                 <div className='h-full w-72 rounded-l-md relative'>
                   <Image 
                     fill
@@ -44,14 +44,14 @@ export default function Home() {
                 </div>
 
                 <div className="w-full flex flex-col gap-2 px-4 py-4 2xl:py-6">
-                  <h2 className="text-2xl text-indigo-400 font-medium">{article.title}</h2>
+                  <h2 className="text-lg 2xl:text-2xl text-indigo-400 font-medium">{article.title}</h2>
                   <p className="flex-grow text-sm 2xl:text-base">{article.excerpt}</p>
-                  <button className='bg-slate-700 hover:bg-indigo-400/40 transition duration-150 ease-linear rounded-lg px-4 py-2 inline max-w-max'>Ler mais</button>
+                  <button className='text-sm 2xl:text-base bg-slate-700 hover:bg-indigo-400/40 transition duration-150 ease-linear rounded-md px-4 py-2 inline max-w-max'>Ler mais</button>
                 </div>
               </div>
             ))}
           </div>
-          <div className="col-span-4 bg-red-900">B</div>
+          <div className="col-span-4 bg-red-900 rounded-md">B</div>
         </div>
       </div>
     </div>
